cac5e7a8ead7418782c0ee257072d8a1d4a57093
Nilesh28v <99234053+Nilesh28v@users.noreply.github.com>
2022-02-09 16:56:26 +0530

    variable practice

---
+++ variable/Addition of string.js
@@ -0,0 +1,2 @@
+let  a=' nilesh ',b='vishwakarma ',c='Happy ';
+console.log("I am "+c ,"and my name is" +a +b);
--- variable/Div.js
+++ variable/Div.js
@@ -1,6 +1,6 @@
 let t;
-let c = 2;
-let b = 3;
-let a = 6;
-t = c * a * b;
+let c = 876;
+let b = 65;
+let a = 87;
+t = c / a  / b;
 console.log("The answer for these 3 variable is "+ t + " and the variables are " +a, + b ,+c);
