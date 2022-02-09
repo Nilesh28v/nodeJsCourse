76d584f6705e40725ce0d0f038d7b2b04ea978c0
Nilesh28v <99234053+Nilesh28v@users.noreply.github.com>
2022-02-09 20:50:00 +0530

    variable practice

--- variable/profitloss.js
+++ variable/profitloss.js
@@ -1,5 +1,8 @@
-let p=5000;
-let a=7000;
+let principle=5000;
+let amount=7000;
 let profit=0;
-profit=a-p;
+profit=amount-principle;
 console.log("Profit of the given amount is= "+ profit);
+amount =4500
+loss=principle-amount;
+console.log("Loss in the given amount is="+loss);
