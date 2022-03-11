e790fcaa710ad651619666a62b8b516a4dda2357
Nilesh28v <99234053+Nilesh28v@users.noreply.github.com>
2022-03-11 08:43:59 +0530

    10-03

--- promise/promise.js
+++ promise/promise.js
@@ -1,9 +1,26 @@
 //promise - its an object in JS, it basically has 3 stages, pending, fulfilled, rejected
 //we use them as an alternative for callbacks in the places where ealy is involved.
 
-console.log('Initial');
-setTimeout(()=> {
-    console.log('Inside');
+function func1() {
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            const error = true;
+            if (!error) {
+                console.log('function: your promise has been resolved');
+                resolve();
+            }
+            else {
+                console.log('Your promise has been not resoled');
+                reject('sorry Rejected');
+            }
 
-},1000);
-console.log('outside');
+        }, 2000);
+
+    })
+}
+
+func1().then(function () {
+    console.log('Nilesh: Thanks for resolving');
+}).catch(function () {
+    console.log('Nilesh: Very bad')
+})
