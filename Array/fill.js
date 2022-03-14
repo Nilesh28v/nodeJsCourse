fca96cae5a64ce6201674d8356eb80a015945fc7
Nilesh28v <99234053+Nilesh28v@users.noreply.github.com>
2022-03-14 22:13:49 +0530

    14-03

---
+++ Array/fill.js
@@ -0,0 +1,10 @@
+var testArray =[2,3,2,5,6,4,8];
+
+console.log(testArray.fill('h',2,5));
+
+
+const myNumber =[23,4,4,3,45,465,34];
+
+const result = myNumber.filter((num) => num !=45);   //in filter callback is needed so we given
+
+console.log(result);
--- Array/splice.js
+++ Array/splice.js
@@ -1,3 +1,8 @@
 const a=[1,2,3,4,5]
 a.splice(1,3);    //remove values from a[2] to a[3] 
 console.log(a);   // we're giving range to delete elements
+
+
+const b=[34,45,3,3,2,2,3,45,3434,435,5,4];
+b.splice(1,4,'hi');  //start from 1, remove  2 values and put 'hi' instead of these values 
+console.log(b);   //we can put more than 1 values instead of 'hi'
