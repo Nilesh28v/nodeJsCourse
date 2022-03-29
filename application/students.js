1628d91813167b646a2d37f14a0cc34ea2044981
Nilesh28v <99234053+Nilesh28v@users.noreply.github.com>
2022-03-29 17:56:36 +0530

    29-03

---
+++ application/students.js
@@ -0,0 +1,57 @@
+const students = 
+
+    [{
+        "id": 1,
+        "first_name": "Alfredo",
+        "last_name": "Gascoigne",
+        "email": "agascoigne0@a8.net"
+      }, {
+        "id": 2,
+        "first_name": "Adlai",
+        "last_name": "Housley",
+        "email": "ahousley1@mail.ru"
+      }, {
+        "id": 3,
+        "first_name": "Manuel",
+        "last_name": "Robertson",
+        "email": "mrobertson2@twitter.com"
+      }, {
+        "id": 4,
+        "first_name": "Evvy",
+        "last_name": "Aronov",
+        "email": "earonov3@uol.com.br"
+      }, {
+        "id": 5,
+        "first_name": "Dodi",
+        "last_name": "Booy",
+        "email": "dbooy4@about.me"
+      }, {
+        "id": 6,
+        "first_name": "Sax",
+        "last_name": "Cloney",
+        "email": "scloney5@wix.com"
+      }, {
+        "id": 7,
+        "first_name": "Slade",
+        "last_name": "Tutsell",
+        "email": "stutsell6@yahoo.co.jp"
+      }, {
+        "id": 8,
+        "first_name": "Blondie",
+        "last_name": "McQuirter",
+        "email": "bmcquirter7@linkedin.com"
+      }, {
+        "id": 9,
+        "first_name": "Keenan",
+        "last_name": "Geoghegan",
+        "email": "kgeoghegan8@github.com"
+      }, {
+        "id": 10,
+        "first_name": "Gustie",
+        "last_name": "Killough",
+        "email": "gkillough9@wiley.com"
+      }]
+      
+
+
+module.exports= students
